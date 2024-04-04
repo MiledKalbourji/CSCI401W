694a1e699f9747ca9abfc2add711459640c35267
dteni <dogunronbi_2001@email.ric.edu>
2024-04-04 11:58:42 -0400

    Added an accounts app

--- Teacher/attendance/serializers.py
+++ Teacher/attendance/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class AttendanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attendance
-        fields =['id','date','present', 'student_name', 'student_id']
+        fields = ['id', 'date', 'present', 'student_name', 'student_id', 'email', 'phone_number', 'time']
